bab71d98b90a0237dfd9e85470ccb42d72e54253
Aaryaman Sagar <aary@users.noreply.github.com>
2016-08-25 23:47:14 -0400

    Travis (#1)
    * updated test case
    
    * added .travis.yml
    
    * modified travis.yml
    
    * Updated compiler for travis
    
    * updated command to use clang
    
    * updated command to use clang
    
    * updated .travis.yml with file from blog post
    
    * added fail test
    
    * changed from  to g++
    
    * fixed travis.yml error

--- test.cpp
+++ test.cpp
@@ -3,10 +3,11 @@
  * A pointer can be cast safely to an integral type as has been shown in the
  * main function without the need for a special uintptr_t type.  All integral
  * types in <cstdint> can technically be replaced with one universal class like
- * the following.  
+ * the following.
  */
 #include <iostream>
 #include <limits>
+#include <cassert>
 #include "Cppstdint.hpp"
 using namespace std;
 using namespace Cppstdint;
@@ -14,14 +15,16 @@ using namespace Cppstdint;
 int main() {
 
     // demonstration of a pointer handle
-    Int<sizeof(void*) * 8, SignType::UNSIGNED>::type int_handle = 
+    Int<sizeof(void*) * 8, SignType::UNSIGNED>::type int_handle =
         (decltype(int_handle)) (void*)(0x444);
-    cout << int_handle << endl;
+    assert(int_handle == 1092);
 
     // demonstration of printing the maximum primitive int value
-    Int<MAX, SignType::UNSIGNED>::type maximum_primitive_int = 
+    Int<MAX, SignType::UNSIGNED>::type maximum_primitive_int =
         std::numeric_limits<decltype(maximum_primitive_int)>::max();
-    cout << maximum_primitive_int << endl;
+    auto max_number =
+        std::numeric_limits<decltype(maximum_primitive_int)>::max();
+    assert(maximum_primitive_int == max_number);
 
     return 0;
 }
